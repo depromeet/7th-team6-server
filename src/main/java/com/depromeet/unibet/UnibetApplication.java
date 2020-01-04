909debe7ba363146cfdde8d50691ace7a6fb7837
jhjhj0366 <bestdeveloper0366@gmail.com>
2020-01-04 17:13:24 +0900

    init project

---
+++ src/main/java/com/depromeet/unibet/UnibetApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.unibet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnibetApplication.class, args);
+    }
+
+}
